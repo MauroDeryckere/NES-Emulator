ffaa37b084a1cb76a61e237a1af08faaf575e04a
Mauro Deryckere <mauro.deryckere@gmail.com>
2025-01-24 08:45:24 +0100

    Added SDL 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
+#include <SDL3/SDL.h>
+//#include <SDL3/SDL_main.h>
+#include <SDL3/SDL_init.h>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hellp World!\n";
+    SDL_Log("%s", "SDL Works");
     return 0;
 }
