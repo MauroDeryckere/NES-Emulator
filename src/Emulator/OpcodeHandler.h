97a67a6f54f0e61ee73e78ef0520a7e5f7888c6f
Mauro Deryckere <mauro.deryckere@gmail.com>
2025-02-02 17:12:35 +0100

    move macros to separate file

--- src/Emulator/OpcodeHandler.h
+++ src/Emulator/OpcodeHandler.h
@@ -2,36 +2,9 @@
 #define NES_EMULATOR_OPCODEHANDLER
 
 
-// Configuration
-#define NES_EM_USE_STATIC_CONSTEXPR_TABLE 1
-#define NES_EM_DEBUG_MODE 1
-
-// Defines required when in debug or other config modes
-#if NES_EM_DEBUG_MODE
-// Initialize the string implementation array for our instructions
-	#define NES_EM_INIT_STRING_REPRESENTATION 1
-#endif
-
-// Compiler specific force inline
-// For MSVC
-#if defined(_MSC_VER)
-	#define FORCE_INLINE __forceinline
-// For GCC and Clang
-#elif defined(__GNUC__) || defined(__clang__)
-	#define FORCE_INLINE __attribute__((always_inline))
-#else
-	#define FORCE_INLINE inline
-#endif
-
-// Allow swapping between static and non static tables
-#if NES_EM_USE_STATIC_CONSTEXPR_TABLE
-	#define NES_EM_TABLE static constexpr
-#else
-	#define NES_EM_TABLE const
-#endif
-
-
 // Includes
+#include "Macros.h"
+
 #include <cstdint>
 #include <array>
 
